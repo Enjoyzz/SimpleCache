dccb12b8badf378028c4b39e7d9728db0d8a6765
Enjoys <deadly.info@gmail.com>
2020-12-09 18:44:20 +0300

    add NullCacher.php

---
+++ src/Cacher/NullCacher.php
@@ -0,0 +1,75 @@
+<?php
+
+
+namespace Enjoys\SimpleCache\Cacher;
+
+
+use Enjoys\SimpleCache\Cacher;
+
+class NullCacher extends Cacher
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function get($key, $default = null)
+    {
+        // TODO: Implement get() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function set($key, $value, $ttl = null)
+    {
+        // TODO: Implement set() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function delete($key)
+    {
+        // TODO: Implement delete() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function clear()
+    {
+        // TODO: Implement clear() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getMultiple($keys, $default = null)
+    {
+        // TODO: Implement getMultiple() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setMultiple($values, $ttl = null)
+    {
+        // TODO: Implement setMultiple() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function deleteMultiple($keys)
+    {
+        // TODO: Implement deleteMultiple() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function has($key)
+    {
+        // TODO: Implement has() method.
+    }
+}
