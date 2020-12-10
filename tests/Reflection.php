efef4a1911408ff5907aa119c138921555c14246
Enjoys <deadly.info@gmail.com>
2020-12-10 16:40:42 +0300

    add getPrivateProperty

--- tests/Reflection.php
+++ tests/Reflection.php
@@ -22,4 +22,21 @@ trait Reflection
 
         return $method;
     }
+
+    /**
+     * getPrivateProperty
+     *
+     * @author	Joe Sexton <joe@webtipblog.com>
+     * @param 	string $className
+     * @param 	string $propertyName
+     * @return	\ReflectionProperty
+     */
+    public function getPrivateProperty($className, $propertyName)
+    {
+        $reflector = new \ReflectionClass($className);
+        $property = $reflector->getProperty($propertyName);
+        $property->setAccessible(true);
+
+        return $property;
+    }
 }
