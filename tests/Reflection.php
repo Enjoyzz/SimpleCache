3ec41a732c8b22e5d785ff590a861ad1e5776b11
Enjoys <deadly.info@gmail.com>
2020-12-10 15:43:32 +0300

    вынесен reflection в трейт

---
+++ tests/Reflection.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Tests\Enjoys\SimpleCache;
+
+
+trait Reflection
+{
+    /**
+     * getPrivateMethod
+     *
+     * @author	Joe Sexton <joe@webtipblog.com>
+     * @param 	string $className
+     * @param 	string $methodName
+     * @return	\ReflectionMethod
+     */
+    public function getPrivateMethod($className, $methodName)
+    {
+        $reflector = new \ReflectionClass($className);
+        $method = $reflector->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method;
+    }
+}
