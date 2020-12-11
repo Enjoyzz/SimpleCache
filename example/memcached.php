7d19dcaba29865389b01ef561e1b51a932e66900
Enjoys <deadly.info@gmail.com>
2020-12-11 15:34:10 +0300

    develope Memcached.php

--- example/memcached.php
+++ example/memcached.php
@@ -1,4 +1,12 @@
 <?php
+
 include __DIR__ . '/../vendor/autoload.php';
 
-$cacher = new \Enjoys\SimpleCache\Cacher\Memcached();
+$cacher = new \Enjoys\SimpleCache\Cacher\Memcached(
+    [
+        'host' => 'memcached'
+    ]
+);
+
+$set = $cacher->set('d1', 2);
+var_dump($set, $cacher->get('d1'));
